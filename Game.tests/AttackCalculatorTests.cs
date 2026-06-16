047b5ee2022c5ef272313cca7e2b860b51687703
Dani H <ctrl.alt.d@gmail.com>
2026-06-16 19:20:59 +0200

    feat: Test SiLaMevaArmaduraEsProuFortaElDanyEs0

--- Game.tests/AttackCalculatorTests.cs
+++ Game.tests/AttackCalculatorTests.cs
@@ -2,10 +2,31 @@ namespace Game.tests;
 
 public class AttackCalculatorTests 
 {
+    const int NoIMPORTA = 0;
     [Fact]
-    public void PodemInstanciarAttackCalculatorTestable()
+    public void SiLaMevaArmaduraEsProuFortaElDanyEs0()
     {          
-        var _ = new AttackCalculatorTestable(12);
+        // arrange
+        var calc = new AttackCalculatorTestable(5);
+        var atk = new Character(
+            armorClass: NoIMPORTA,
+            weaponDamage: NoIMPORTA,
+            race: "Atacant",
+            force: 5);
+        var def = new Character(
+            armorClass: 100,
+            weaponDamage: NoIMPORTA,
+            race: "Defensor",
+            force: NoIMPORTA);
+
+        // act
+        var resultat = calc.CalculateDamage(atk, def);
+
+        // assert
+        Assert.Equal(0, resultat);
     }
+
+
+
 
 }
